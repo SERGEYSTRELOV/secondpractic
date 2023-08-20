61df1971728d0f169de253a6d9843326dd2ae2d2
sergeystrelov <strelov09@mail.ru>
2023-08-20 16:10:26 +0300

    задача 15

--- Program.cs
+++ Program.cs
@@ -34,14 +34,33 @@
 //string stringNumber = Convert.ToString(threeDigitNumber);
 //Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
 
-Console.WriteLine("Задача 13 ");
+//Console.WriteLine("Задача 13 ");
 
-Console.Write("Введи число: ");
-int anyNumber = Convert.ToInt32(Console.ReadLine());
-string anyNumberText = Convert.ToString(anyNumber);
-if (anyNumberText.Length > 2){
-  Console.WriteLine("третья цифра -> " + anyNumberText[2]);
+//Console.Write("Введи число: ");
+//int anyNumber = Convert.ToInt32(Console.ReadLine());
+//string anyNumberText = Convert.ToString(anyNumber);
+//if (anyNumberText.Length > 2){
+  //Console.WriteLine("третья цифра -> " + anyNumberText[2]);
+//}
+//else {
+ // Console.WriteLine("-> третьей цифры нет");
+//}
+
+Console.WriteLine("Задача 15 ");
+
+Console.Write("Введи цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek (int dayNumber) {
+  if (dayNumber == 6 || dayNumber == 7) {
+  Console.WriteLine("(этот день выходной) -> да");
+  }
+  else if (dayNumber < 1 || dayNumber > 7) {
+    Console.WriteLine("это вообще не день недели");
+  }
+  else Console.WriteLine("(этот день не выходной) -> нет");
 }
-else {
-  Console.WriteLine("-> третьей цифры нет");
-}
+
+CheckingTheDayOfTheWeek(dayNumber);
+
+
